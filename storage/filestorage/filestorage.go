0a9396ee66bec2d9e2f8c962281d3360a60aa523
differz <differ@xlan.net.ua>
2018-04-04 16:54:34 +0300

    rename

--- storage/filestorage/filestorage.go
+++ storage/filestorage/filestorage.go
@@ -23,28 +23,28 @@ func (s Storage) StoreItem(item core.Item) {
 }
 
 // Get file from storage
-func (s Storage) FindItemById(id int) {
-
+func (s Storage) FindItemById(id int) core.Item {
+	return core.Item{}
 }
 
 // Save bucket to storage
-func (s Storage) StoreOrder(item core.Item) {
+func (s Storage) StoreOrder(item core.Order) {
 
 }
 
 // Get bucket from storage
-func (s Storage) FindOrderById(id int) {
-
+func (s Storage) FindOrderById(id int) core.Order {
+	return core.Order{}
 }
 
 // Save client to storage
-func (s Storage) StoreCustomer(item core.Item) {
+func (s Storage) StoreCustomer(item core.Customer) {
 
 }
 
 // Get client from storage
-func (s Storage) FindCustomerById(id int) {
-
+func (s Storage) FindCustomerById(id int) core.Customer {
+	return core.Customer{}
 }
 
 func init() {
