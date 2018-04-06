9657bd6a78f50169916dcd3f3b7bf3f11f271bbb
differz <differ@xlan.net.ua>
2018-04-06 20:29:03 +0300

    place order

--- usecases/placeorderusecase.go
+++ usecases/placeorderusecase.go
@@ -1,5 +1,26 @@
 package usecases
 
-func PlaceOrderUseCase() {
+import "../contracts/usecases"
 
+type PlaceOrderUseCase struct {
+	projectId   int64
+	subject     string
+	description string
+}
+
+func NewPlaceOrderUseCase() PlaceOrderUseCase {
+	return PlaceOrderUseCase{
+		projectId: 1,
+	}
+}
+
+func (u *PlaceOrderUseCase) placeOrder(request contracts.PlaceOrderRequest, output contracts.PlaceOrderOutput) {
+	/*      int projectId = request.getProjectId();
+	        String subject = request.getSubject();
+	        String description = request.getDescription();
+
+	        orderRepository.saveIssue(issue);
+
+			output.onResponse(orderID);
+	*/
 }
