e78be08c407d5a2af258a75de2204a95748d17ba
differz <differ@xlan.net.ua>
2018-05-11 13:34:45 +0300

    ref

--- messenger/telegram/component.go
+++ messenger/telegram/component.go
@@ -38,14 +38,14 @@ func (m *Messenge) Init(args ...string) error {
 	return err
 }
 
-// Name telegram
-func (m Messenge) Name() string {
-	return m.name
-}
-
 // Available true if telegram api is on
 func (m Messenge) Available() bool {
 	return m.bot != nil
+}
+
+// Name telegram
+func (m Messenge) Name() string {
+	return m.name
 }
 
 func (m Messenge) String() string {
